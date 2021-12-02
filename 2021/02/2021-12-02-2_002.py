7dcc44a860dbb0df0fb8c08088fa443ede6dd0a6
james.bensley <james.bensley@sky.uk>
2021-12-02 20:21:00 +0100

    Adding 2021 day 2 part 2 solutions

---
+++ 2021/02/2021-12-02-2_002.py
@@ -0,0 +1,20 @@
+"""
+Lines of code: medium
+Readability: medium
+Efficiency: high
+
+9 lines of code, quite readable, walks the data set only once.
+"""
+
+aim, horiz, depth = 0, 0, 0
+for line in open("input"):
+
+    if line.split()[0] == "up":
+        aim -= int(line.split()[1])
+    elif line.split()[0] == "down":
+        aim += int(line.split()[1])
+    else:
+        horiz += int(line.split()[1])
+        depth += aim * int(line.split()[1])
+
+print(f"{horiz*depth}")
